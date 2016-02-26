fe853a357e50f59a370e8a5e290f86dbacb73d0a
BryanPark <forcecore.acidb@gmail.com>
2016-02-26 12:07:30 +0900

    Revert "first snapshot"
    This reverts commit 612fa508992ddb67d243605562f569f00e480ac8.

--- __practice/ajaxmvc/model/business.php
+++
@@ -1,6 +0,0 @@
-<?php
-function loadSomething() {
-	$extensions = get_loaded_extensions();
-	return '["'. implode('","', $extensions) . '"]'; 
-}
-?>
--- controller/logout.php
+++
@@ -1,7 +0,0 @@
-<?
-#세션이용시 필요
-session_start();
-#세션파기
-session_destroy();
-#게시판으로 되돌아감#return to the board
-?>
